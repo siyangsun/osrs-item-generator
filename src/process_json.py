722a7a72c8aed44fc194fbff035160f252b0e550
siyangsun <siyangsun2007@gmail.com>
2019-08-04 19:10:26 -0700

    get all item names from json

--- src/process_json.py
+++ src/process_json.py
@@ -1,1 +1,12 @@
 import json
+
+
+def get_itemlist():
+    names = []
+    with open('../data/raw/items-summary.json') as json_file:
+        data = json.load(json_file)
+        for i in data.keys():
+            item = data[str(i)]
+            names.append(item['name'])
+    json_file.close()
+    return(names)
